9fb38adfa641cdb2faa61e417fc1b577e3ea6ab1
unknown <zach.jilani1@gmail.com>
2023-04-18 21:17:20 -0700

    bruh this costs a whole 4 cents

--- api.py
+++ api.py
@@ -2,8 +2,17 @@ import os
 from dotenv import load_dotenv
 import openai
 
-
+prompt = 'a white cat'
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 model = openai.Model.retrieve("text-davinci-003")
 print(model)
+res = openai.Image.create(
+    prompt=prompt,
+    n=1,
+    size='1024x1024'
+)
+
+url = res['data'][0]['url']
+
+print(url)
